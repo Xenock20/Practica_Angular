599635c547abb39ca5f66ed971070a36093034da
Xenock20 <juliosaboredo@gmail.com>
2022-12-03 17:18:16 -0300

    Bases Aprendidas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './product.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,69 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-store';
+  name: string = 'Federico';
+  age: number = 18;
+  img: string = 'https://www.w3schools.com/howto/img_avatar.png';
+  btnDisable: boolean = true;
+  persona = {
+    name: 'Federico',
+    age: 18,
+    avatar: 'https://www.w3schools.com/howto/img_avatar.png'
+  }
+  nombres: string[] = ['Federico', 'Agustin', 'Julian', 'Analia']
+  newName = '';
+  products: Product[] = [
+    {
+      name: 'iPhone 5',
+      price: 1.99,
+      img: './assets/img/i5.jpg'
+    },
+    {
+      name: 'iPhone 6',
+      price: 1.99,
+      img: './assets/img/i6.jpg'
+    },
+    {
+      name: 'iPhone 7',
+      price: 1.99,
+      img: './assets/img/i7.jpg'
+    },
+    {
+      name: 'iPhone 8',
+      price: 1.99,
+      img: './assets/img/i8.jpg'
+    },
+    {
+      name: 'iPhone X',
+      price: 1.99,
+      img: './assets/img/ix.jpg'
+    },
+  ];
+
+  toggleButton(): void {
+    this.btnDisable = !this.btnDisable;
+  }
+
+  addAge(): void {
+    this.persona.age++;
+  }
+
+  onScroll(event: Event): void {
+    const elemento = event.target as HTMLElement;
+    console.log(elemento.scrollTop);
+  }
+
+  changeName(event: Event): void{
+    const elemento = event.target as HTMLInputElement;
+    this.persona.name = elemento.value;
+  }
+
+  addName(): void {
+    this.nombres.push(this.newName);
+    this.newName = '';
+  }
+
+  deleteName(i:number): void {
+    this.nombres.splice(i, 1);
+  }
 }
