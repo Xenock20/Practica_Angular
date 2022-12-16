9256ccd20febc4add49d8b24b5cab2e76892ce46
Xenock20 <juliosaboredo@gmail.com>
2022-12-16 15:22:00 -0300

    First peticion get

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -11,10 +11,13 @@ export class ProductComponent {
   @Input() product: Product = {
     id: '',
     title: '' ,
-    image: 'https://crocsegypt.com/Content/images/NO_image.jpg',
+    images: [],
     price: 0,
     description: '',
-    category: ''
+    category: {
+      id: '',
+      name: '',
+    }
   }
 
   @Output() addedProduct = new EventEmitter<Product>();
